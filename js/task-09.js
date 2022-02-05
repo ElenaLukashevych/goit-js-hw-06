14f2872dec222b3fd3dd0ad493363fbb9c9f8a16
ElenaLukashevych <elukashevych@gmail.com>
2022-02-05 17:01:50 +0200

    tesk9

--- js/task-09.js
+++ js/task-09.js
@@ -2,3 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const buttonEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  document.body.style.background =  getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+}
+
+
