40d8ffbfba8b8f31c5c7965b1e37bc2e3868d359
ElenaLukashevych <elukashevych@gmail.com>
2022-02-05 18:13:26 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+
+    const formElements = event.currentTarget;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+        mail,
+        password,
+    }
+
+    if (mail === "" || password === "") {
+        const message = "Все поля должны быть заполнены!";
+        alert(message);
+    } else {
+        console.log(formData);
+
+    
+        formEl.reset();
+    };
+};
 
